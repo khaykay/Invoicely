845a7e24f91d7de0a63ae06e1a6a847c2a2ad92a
khaykay <berryscott97@gmail.com>
2024-02-07 22:45:57 +0100

    cleanedcolor

--- src/components/IconComponents/Color.jsx
+++ src/components/IconComponents/Color.jsx
@@ -4,20 +4,22 @@ import { IoAddCircleOutline } from "react-icons/io5";
 const Color = () => {
   const [selectedColor, setSelectedColor] = useState("#000000");
   const [isColorPickerVisible, setColorPickerVisible] = useState(false);
-  const [recentlyUsedColors, setRecentlyUsedColors] = useState([selectedColor]);
-
+  const cleanedColor = selectedColor.replace(/["]/g, " ");
+  console.log(typeof cleanedColor + "clean");
+  const [recentlyUsedColors, setRecentlyUsedColors] = useState([cleanedColor]);
   if (recentlyUsedColors.length == 8) {
     recentlyUsedColors.shift();
   }
 
   const handleColorChange = (event) => {
     const color = event.target.value;
-    setSelectedColor(color);
+    setSelectedColor(cleanedColor);
     setColorPickerVisible(true);
     setRecentlyUsedColors((prevColors) => [...prevColors, color]);
   };
   // console.log(selectedColor + "it happened 2");
   console.log(recentlyUsedColors);
+
   return (
     <>
       <div className="relative bg-gradient-to-b from-red-500 via-yellow-500 to-green-500 h-12 w-12 rounded-full">
@@ -50,8 +52,11 @@ const Color = () => {
         <h5 className="mb-3 text-xs  ">Recently used color</h5>
         {/* <span className=""></span> */}
         <div className="">
-          {recentlyUsedColors.map((color, index) => (
-            <span className={`h-12 w-12 rounded-full bg-${color}`} key={index}>
+          {recentlyUsedColors?.map((color, index) => (
+            <span
+              className={`h-12 w-12 rounded-full bg-[${color}]`}
+              key={index}
+            >
               recently used {color}
             </span>
           ))}
